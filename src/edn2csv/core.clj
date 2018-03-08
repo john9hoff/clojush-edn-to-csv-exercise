efd0179b4be871c9d7d0491b20a30d307e8b697f
CMenne <menne019@morris.umn.edu>
2018-03-07 18:50:23 -0600

    Add print-semantics-to-csv and print-parentOfEdges-to-csv
    We believe our parentOfEdges function works
    Not sure on our semantics functions works

--- src/edn2csv/core.clj
+++ src/edn2csv/core.clj
@@ -9,7 +9,14 @@
 
 ; The header line for the Individuals CSV file
 (def individuals-header-line "UUID:ID(Individual),Generation:int,Location:int,:LABEL")
+(def parentOfEdges-header-line ":START_ID(Individual), GeneticOperator:string, :END_ID(Individual), :TYPE")
+(def semantics-header-line "UUID:ID(Semantics), TotalError:int, :LABEL")
 
+; Generate UUIDs for semantics
+(defn generate-uuid [] (str (java.util.UUID/randomUUID)))
+
+; Atom for semantics
+(def semantics-atom (atom {}))
 ; Ignores (i.e., returns nil) any EDN entries that don't have the
 ; 'clojure/individual tag.
 (defn individual-reader
@@ -37,6 +44,48 @@
     (apply safe-println csv-file $))
   1)
 
+(defn print-semantics-to-csv
+  [csv-file line]
+  (when-not (contains? @semantics-atom (get line :errors))
+    (let [semantic-uuid (generate-uuid)
+          values [semantic-uuid (get line :total-error) "Semantics"]]
+        (swap! semantics-atom assoc (get line :errors) (get line :total-error))
+        (apply safe-println csv-file values)))
+  1)
+
+
+(defn print-parentOfEdges-to-csv
+  [csv-file line]
+  (as-> line $
+    (map $ [:parent-uuids :genetic-operators :uuid])
+    (concat $ ["PARENT_OF"])
+    (apply safe-println csv-file $))
+  1)
+
+(defn edn->csv-sequential-parentOfEdges [edn-file csv-file]
+  (with-open [out-file (io/writer csv-file)]
+    (safe-println out-file parentOfEdges-header-line)
+    (->>
+      (line-seq (io/reader edn-file))
+      ; Skip the first line because it's not an individual
+      (drop 1)
+      (map (partial edn/read-string {:default individual-reader}))
+      (map (partial print-parentOfEdges-to-csv out-file))
+      (reduce +)
+      )))
+
+(defn edn->csv-sequential-semantics [edn-file csv-file]
+  (with-open [out-file (io/writer csv-file)]
+    (safe-println out-file semantics-header-line)
+    (->>
+      (line-seq (io/reader edn-file))
+      ; Skip the first line because it's not an individual
+      (drop 1)
+      (map (partial edn/read-string {:default individual-reader}))
+      (map (partial print-semantics-to-csv out-file))
+      (reduce +)
+      )))
+
 (defn edn->csv-sequential [edn-file csv-file]
   (with-open [out-file (io/writer csv-file)]
     (safe-println out-file individuals-header-line)
@@ -49,6 +98,19 @@
       (reduce +)
       )))
 
+(defn edn->csv-pmap-parentOfEdges [edn-file csv-file]
+  (with-open [out-file (io/writer csv-file)]
+    (safe-println out-file parentOfEdges-header-line)
+    (->>
+      (line-seq (io/reader edn-file))
+      ; Skip the first line because it's not an individual
+      (drop 1)
+      (pmap (fn [line]
+        (print-parentOfEdges-to-csv out-file (edn/read-string {:default individual-reader} line))
+        1))
+      count
+      )))
+
 (defn edn->csv-pmap [edn-file csv-file]
   (with-open [out-file (io/writer csv-file)]
     (safe-println out-file individuals-header-line)
@@ -58,6 +120,19 @@
       (drop 1)
       (pmap (fn [line]
         (print-individual-to-csv out-file (edn/read-string {:default individual-reader} line))
+        1))
+      count
+      )))
+
+(defn edn->csv-pmap-semantics [edn-file csv-file]
+  (with-open [out-file (io/writer csv-file)]
+    (safe-println out-file semantics-header-line)
+    (->>
+      (line-seq (io/reader edn-file))
+      ; Skip the first line because it's not an individual
+      (drop 1)
+      (pmap (fn [line]
+        (print-semantics-to-csv out-file (edn/read-string {:default individual-reader} line))
         1))
       count
       )))
@@ -88,6 +163,26 @@
          "_sequential")
        "_Individuals.csv"))
 
+(defn build-parentOfEdges-csv-filename
+ [edn-filename strategy]
+ (str (fs/parent edn-filename)
+      "/"
+      (fs/base-name edn-filename ".edn")
+      (if strategy
+        (str "_" strategy)
+        "_sequential")
+      "_ParentOfEdges.csv"))
+
+(defn build-semantics-csv-filename
+  [edn-filename strategy]
+  (str (fs/parent edn-filename)
+       "/"
+       (fs/base-name edn-filename ".edn")
+       (if strategy
+         (str "_" strategy)
+         "_sequential")
+       "_Semantics.csv"))
+
 (defn -main
   [edn-filename & [strategy]]
   (let [individual-csv-file (build-individual-csv-filename edn-filename strategy)]
@@ -97,6 +192,20 @@
         "pmap" (edn->csv-pmap edn-filename individual-csv-file)
         "reducers" (edn->csv-reducers edn-filename individual-csv-file)
         (edn->csv-sequential edn-filename individual-csv-file))))
+
+
+  (let [parentOfEdges-csv-file (build-parentOfEdges-csv-filename edn-filename strategy)]
+    (time
+      (condp = strategy
+        "sequential" (edn->csv-sequential-parentOfEdges edn-filename parentOfEdges-csv-file)
+        "pmap" (edn->csv-pmap-parentOfEdges edn-filename parentOfEdges-csv-file)
+        (edn->csv-sequential-parentOfEdges edn-filename parentOfEdges-csv-file))))
+
+  (let [semantics-csv-file (build-semantics-csv-filename edn-filename strategy)]
+    (time
+      (condp = strategy
+        "pmap" (edn->csv-pmap-semantics edn-filename semantics-csv-file)
+        (edn->csv-pmap-semantics edn-filename semantics-csv-file))))
   ; Necessary to get threads spun up by `pmap` to shutdown so you get
   ; your prompt back right away when using `lein run`.
   (shutdown-agents))
